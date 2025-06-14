a1e2e8eea4215c726caf8bfbf1353c3166292918
s4myam <humagainsamyam@gmail.com>
2025-06-14 07:55:50 +0545

    6/14/2025 THE BEGINNING

---
+++ hello.py
@@ -0,0 +1,14 @@
+if 5 > 2:
+ print("Five is greater then two!")
+
+ if 5>2 :
+    print("Five is greater than two!!")
+
+    #This is a comment
+
+    print("Hello World")
+
+    x=5
+    y="hello,world!!"
+    print(y)
+    print(x)
---
+++ variables.py
@@ -0,0 +1,7 @@
+x=5
+y="John"
+print(x)
+
+
+y=str(3)
+print(y)
